8b83ec460b8fa93ef19d2f5f45a98ff683843a76
LCLP <lukas@lclpnet.work>
2020-09-27 17:17:06 +0200

    character delete button now working

--- LCLPMMO/src/main/java/work/lclpnet/mmo/gui/character/CharacterChooserScreen.java
+++ LCLPMMO/src/main/java/work/lclpnet/mmo/gui/character/CharacterChooserScreen.java
@@ -6,9 +6,14 @@ import java.util.Objects;
 import java.util.function.Consumer;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screen.ConfirmScreen;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.toasts.SystemToast;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import work.lclpnet.mmo.facade.JsonSerializeable;
 import work.lclpnet.mmo.facade.character.MMOCharacter;
@@ -19,7 +24,7 @@ import work.lclpnet.mmo.util.LCLPNetwork;
 public class CharacterChooserScreen extends EditableGenericSelectionScreen<MMOCharacter> {
 
 	protected final List<MMOCharacter> characters;
-	
+
 	protected CharacterChooserScreen(Screen prevScreen, List<MMOCharacter> characters) {
 		super(new TranslationTextComponent("mmo.menu.select_character.title"), prevScreen);
 		this.characters = Objects.requireNonNull(characters);
@@ -53,7 +58,41 @@ public class CharacterChooserScreen extends EditableGenericSelectionScreen<MMOCh
 
 	@Override
 	public void deleteEntry(MMOCharacter character) {
-		
+		if(character.id == null) throw new NullPointerException("Character id is null.");
+
+		this.minecraft.displayGuiScreen(new ConfirmScreen(yes -> {
+			if(yes) {
+				JsonObject body = new JsonObject();
+				body.addProperty("characterId", character.id);
+				LCLPNetwork.sendRequest("api/ls5/delete-character", "DELETE", body, response -> {
+					if(response.isNoConnection()) {
+						SystemToast.addOrUpdate(this.minecraft.getToastGui(), SystemToast.Type.WORLD_BACKUP,
+								new TranslationTextComponent("mmo.menu.select_character.delete_failed"),
+								new TranslationTextComponent("mmo.no_internet"));
+					} else if(response.getResponseCode() == 200) {
+						SystemToast.addOrUpdate(this.minecraft.getToastGui(), SystemToast.Type.WORLD_BACKUP,
+								new TranslationTextComponent("mmo.menu.select_character.delete_success"),
+								null);
+					} else {
+						ITextComponent reason = new TranslationTextComponent("error.unknown");
+						System.err.println(response);
+						if(response.hasValidationViolations()) {
+							reason = new StringTextComponent(response.getValidationViolations().getFirst());
+						}
+
+						SystemToast.addOrUpdate(this.minecraft.getToastGui(), SystemToast.Type.WORLD_BACKUP,
+								new TranslationTextComponent("mmo.menu.select_character.delete_failed"),
+								reason);
+					}
+
+					CharacterChooserScreen.updateContentAndShow(minecraft, prevScreen);
+				});
+			} else {
+				this.minecraft.displayGuiScreen(CharacterChooserScreen.this);
+			}
+		}, 
+				new TranslationTextComponent("mmo.menu.select_character.confirm_delete"), 
+				new TranslationTextComponent("mmo.menu.select_character.confirm_delete_desc", character.getTitle().getFormattedText())));
 	}
 
 	@Override
@@ -64,16 +103,16 @@ public class CharacterChooserScreen extends EditableGenericSelectionScreen<MMOCh
 	public static void updateContentAndShow(final Minecraft mc, Screen prevScreen) {
 		final Consumer<List<MMOCharacter>> callback = characters -> {
 			final CharacterChooserScreen guiScreenIn = new CharacterChooserScreen(prevScreen, characters);
-			
+
 			Enqueuer.enqueueOnRender(() -> mc.displayGuiScreen(guiScreenIn));
 		};
-		
+
 		LCLPNetwork.post("api/ls5/get-characters", null, response -> {
 			if(response.isNoConnection() || response.getResponseCode() != 200) {
 				callback.accept(new ArrayList<>());
 				return;
 			}
-			
+
 			List<MMOCharacter> characters = new ArrayList<>();
 
 			JsonArray arr = JsonSerializeable.parse(response.getRawResponse(), JsonArray.class);
