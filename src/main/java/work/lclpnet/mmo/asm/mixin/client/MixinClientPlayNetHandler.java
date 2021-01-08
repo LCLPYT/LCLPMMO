c390372a1c9d2b1feebbd659733a21b30e20c143
LCLP <lukas@lclpnet.work>
2021-01-08 18:38:33 +0100

    Player properties not loading fixed

--- src/main/java/work/lclpnet/mmo/asm/mixin/client/MixinClientPlayNetHandler.java
+++ src/main/java/work/lclpnet/mmo/asm/mixin/client/MixinClientPlayNetHandler.java
@@ -6,20 +6,19 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import com.google.gson.JsonObject;
 
-import net.minecraft.client.entity.player.AbstractClientPlayerEntity;
+import net.minecraft.client.entity.player.RemoteClientPlayerEntity;
 import net.minecraft.client.network.play.ClientPlayNetHandler;
-import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ai.attributes.Attribute;
 import net.minecraft.entity.ai.attributes.AttributeModifierManager;
 import net.minecraft.entity.ai.attributes.ModifiableAttributeInstance;
 import net.minecraft.network.play.server.SEntityPropertiesPacket;
+import net.minecraft.network.play.server.SSpawnPlayerPacket;
 import net.minecraft.util.text.TranslationTextComponent;
 import work.lclpnet.mmo.asm.type.IMMOUser;
 import work.lclpnet.mmo.facade.User;
@@ -50,18 +49,20 @@ public class MixinClientPlayNetHandler {
 		if(attr.equals(MMOMonsterAttributes.SCALE_HEIGHT) || attr.equals(MMOMonsterAttributes.SCALE_WIDTH)) entity.recalculateSize();
 	}
 
-	@Redirect(
+	@Inject(
 			method = "Lnet/minecraft/client/network/play/ClientPlayNetHandler;handleSpawnPlayer(Lnet/minecraft/network/play/server/SSpawnPlayerPacket;)V",
 			at = @At(
 					value = "INVOKE",
 					target = "Lnet/minecraft/client/world/ClientWorld;addPlayer("
 							+ "ILnet/minecraft/client/entity/player/AbstractClientPlayerEntity;"
 							+ ")V"
-					)
+					),
+			locals = LocalCapture.CAPTURE_FAILHARD
 			)
-	public void onHandleSpawnPlayer(ClientWorld world, int playerId, AbstractClientPlayerEntity playerEntityIn) {
+	public void onHandleSpawnPlayer(SSpawnPlayerPacket packetIn, CallbackInfo ci, double d0, double d1, double d2, float f, float f1, int i, 
+			RemoteClientPlayerEntity remoteclientplayerentity) {
 		JsonObject body = new JsonObject();
-		body.addProperty("uuid", playerEntityIn.getGameProfile().getId().toString());
+		body.addProperty("uuid", remoteclientplayerentity.getGameProfile().getId().toString());
 
 		LCLPNetwork.post("api/ls5/get-active-character-by-uuid", body, response -> {
 			final MMOCharacter character = User.handleActiveCharacterResponse(response);
@@ -80,11 +81,9 @@ public class MixinClientPlayNetHandler {
 					return;
 				}
 
-				IMMOUser mmo = IMMOUser.getMMOUser(playerEntityIn);
+				IMMOUser mmo = IMMOUser.getMMOUser(remoteclientplayerentity);
 				mmo.setUser(user);
 				mmo.setMMOCharacter(character);
-
-				world.addPlayer(playerId, playerEntityIn);	
 			});
 		});
 	}
