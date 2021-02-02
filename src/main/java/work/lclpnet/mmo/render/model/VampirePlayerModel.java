79046ba8906fe1eba7a8b79602ef8007613ee3f7
LCLP <lukas@lclpnet.work>
2021-02-02 13:51:04 +0100

    vampire model improved

--- src/main/java/work/lclpnet/mmo/render/model/VampirePlayerModel.java
+++ src/main/java/work/lclpnet/mmo/render/model/VampirePlayerModel.java
@@ -1,8 +1,6 @@
 package work.lclpnet.mmo.render.model;
 
 import com.google.common.collect.ImmutableList;
-import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.vertex.IVertexBuilder;
 
 import net.minecraft.client.renderer.model.ModelRenderer;
 import net.minecraft.util.ResourceLocation;
@@ -10,10 +8,10 @@ import work.lclpnet.mmo.LCLPMMO;
 
 public class VampirePlayerModel extends AbstractMMOPlayerModel {
 
-	private ModelRenderer tooth_left;
-	private ModelRenderer tooth_right;
-	private ModelRenderer ear_left;
-	private ModelRenderer ear_right;
+	private ModelRenderer toothLeft;
+	private ModelRenderer toothRight;
+	private ModelRenderer earLeft;
+	private ModelRenderer earRight;
 	
 	public VampirePlayerModel() {
 		super(0F, false);
@@ -25,27 +23,33 @@ public class VampirePlayerModel extends AbstractMMOPlayerModel {
 	}
 
 	private void populate() {
+		bipedHead = new ModelRenderer(this);
+		bipedHead.setRotationPoint(0.0F, 0.0F, 0.0F);
+		bipedHead.setTextureOffset(0, 0).addBox(-4.0F, -8.0F, -4F, 8.0F, 8.0F, 8.0F, 0.0F, false);
+		
+		bipedHeadwear = new ModelRenderer(this);
+		bipedHeadwear.setRotationPoint(0.0F, 0.0F, 0.0F);
+		bipedHeadwear.setTextureOffset(11, 6).addBox(-4.0F, -5.0F, -4.5F, 1.0F, 1.0F, 0.5F, 0.0F, false);
+		bipedHeadwear.setTextureOffset(11, 5).addBox(-2.0F, -5.0F, -4.5F, 1.0F, 1.0F, 0.5F, 0.0F, false);
+		bipedHeadwear.setTextureOffset(11, 5).addBox(1.0F, -5.0F, -4.5F, 1.0F, 1.0F, 0.5F, 0.0F, false);
+		bipedHeadwear.setTextureOffset(11, 5).addBox(3.0F, -5.0F, -4.5F, 1.0F, 1.0F, 0.5F, 0.0F, false);
+		bipedHeadwear.setTextureOffset(16, 9).addBox(-4.0F, -7.0F, -4.5F, 8.0F, 2.0F, 0.5F, 0.0F, false);
+		bipedHeadwear.setTextureOffset(20, 9).addBox(-3.0F, -8.0F, -4.5F, 6.0F, 1.0F, 0.5F, 0.0F, false);
+		
 		bipedBody = new ModelRenderer(this);
 		bipedBody.setRotationPoint(0.0F, 0.0F, 0.0F);
 		bipedBody.setTextureOffset(16, 16).addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, 0.0F, false);
-
-		bipedRightLeg = new ModelRenderer(this);
-		bipedRightLeg.setRotationPoint(-1.9F, 12.0F, 0.0F);
-		bipedRightLeg.setTextureOffset(16, 48).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, false);
-
-		bipedLeftLeg = new ModelRenderer(this);
-		bipedLeftLeg.setRotationPoint(1.9F, 12.0F, 0.0F);
-		bipedLeftLeg.setTextureOffset(16, 48).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, false);
-
-		bipedRightArm = new ModelRenderer(this);
-		bipedRightArm.setRotationPoint(-5F, 2.0F, 0.0F);
-		bipedRightArm.setTextureOffset(40, 16).addBox(-3F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, false);
-
-		bipedRightArmwear = new ModelRenderer(this);
-		bipedRightArmwear.setRotationPoint(-5F, 2F, 0.0F);
-		bipedRightArmwear.setTextureOffset(17, 37).addBox(-3F, -2.0F, -2.5F, 4.0F, 10.0F, 0.5F, 0.0F, false);
-		bipedRightArmwear.setTextureOffset(64, 16).addBox(-3.5F, -2.0F, -2.0F, 0.5F, 10.0F, 4.0F, 0.0F, false);
-		bipedRightArmwear.setTextureOffset(27, 37).addBox(-3F, -2.0F, 2.0F, 4.0F, 10.0F, 0.5F, 0.0F, true);
+		
+		bipedBodyWear = new ModelRenderer(this);
+		bipedBodyWear.setRotationPoint(0.0F, 0.0F, 0.0F);
+		bipedBodyWear.setTextureOffset(40, 0).addBox(-2.0F, 1.0F, -2.5F, 4.0F, 1.0F, 1.0F, 0.0F, false);
+		bipedBodyWear.setTextureOffset(56, 0).addBox(-2.0F, 2.0F, -2.5F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+		bipedBodyWear.setTextureOffset(51, 0).addBox(1.0F, 2.0F, -2.5F, 1.0F, 1.0F, 1.0F, 0.0F, false);
+		bipedBodyWear.setTextureOffset(1, 37).addBox(2.0F, 0.0F, -2.5F, 2.0F, 12.0F, 0.5F, 0.0F, false);
+		bipedBodyWear.setTextureOffset(65, 37).addBox(-4.0F, 0.0F, 2.0F, 8.0F, 12.0F, 0.5F, 0.0F, false);
+		bipedBodyWear.setTextureOffset(7, 37).addBox(-4.0F, 0.0F, -2.5F, 2.0F, 12.0F, 0.5F, 0.0F, false);
+		bipedBodyWear.setTextureOffset(64, 0).addBox(4.001F, 0.0F, -2.0F, 0.0F, 12.0F, 4.0F, 0.0F, false);
+		bipedBodyWear.setTextureOffset(72, 0).addBox(-4.001F, 0.0F, -2.0F, 0.0F, 12.0F, 4.0F, 0.0F, false);
 
 		bipedLeftArm = new ModelRenderer(this);
 		bipedLeftArm.setRotationPoint(5F, 2.0F, 0.0F);
@@ -56,55 +60,52 @@ public class VampirePlayerModel extends AbstractMMOPlayerModel {
 		bipedLeftArmwear.setTextureOffset(37, 37).addBox(-1F, -2.0F, -2.5F, 4.0F, 10.0F, 0.5F, 0.0F, false);
 		bipedLeftArmwear.setTextureOffset(73, 16).addBox(3F, -2.0F, -2.0F, 0.5F, 10.0F, 4.0F, 0.0F, false);
 		bipedLeftArmwear.setTextureOffset(47, 37).addBox(-1F, -2.0F, 2.0F, 4.0F, 10.0F, 0.5F, 0.0F, true);
-
-		bipedHead = new ModelRenderer(this);
-		bipedHead.setRotationPoint(0.0F, 0.0F, 0.0F);
-		bipedHead.setTextureOffset(0, 0).addBox(-4.0F, -8.0F, -4F, 8.0F, 8.0F, 8.0F, 0.0F, false);
+		bipedLeftArmwear.setTextureOffset(68, 50).addBox(-1.001F, -2.0F, -2.0F, 0.0F, 10.0F, 4.0F, 0.0F, false);
 		
-		tooth_left = new ModelRenderer(this);
-		tooth_left.setRotationPoint(0.0F, 5.0F, -1.0F);
-		bipedHead.addChild(tooth_left);
-		tooth_left.setTextureOffset(44, 25).addBox(1.0F, -6.0F, -3.5F, 1.0F, 1.0F, 0.5F, 0.0F, false);
-		tooth_left.setTextureOffset(44, 27).addBox(1.5F, -5.0F, -3.5F, 0.5F, 0.5F, 0.5F, 0.0F, false);
+		bipedRightArm = new ModelRenderer(this);
+		bipedRightArm.setRotationPoint(-5F, 2.0F, 0.0F);
+		bipedRightArm.setTextureOffset(40, 16).addBox(-3F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, false);
 
-		tooth_right = new ModelRenderer(this);
-		tooth_right.setRotationPoint(0.0F, 5.0F, -1.0F);
-		bipedHead.addChild(tooth_right);
-		tooth_right.setTextureOffset(43, 25).addBox(-2.0F, -6.0F, -3.5F, 1.0F, 1.0F, 0.5F, 0.0F, false);
-		tooth_right.setTextureOffset(43, 26).addBox(-2.0F, -5.0F, -3.5F, 0.5F, 0.5F, 0.5F, 0.0F, false);
+		bipedRightArmwear = new ModelRenderer(this);
+		bipedRightArmwear.setRotationPoint(-5F, 2F, 0.0F);
+		bipedRightArmwear.setTextureOffset(17, 37).addBox(-3F, -2.0F, -2.5F, 4.0F, 10.0F, 0.5F, 0.0F, false);
+		bipedRightArmwear.setTextureOffset(64, 16).addBox(-3.5F, -2.0F, -2.0F, 0.5F, 10.0F, 4.0F, 0.0F, false);
+		bipedRightArmwear.setTextureOffset(27, 37).addBox(-3F, -2.0F, 2.0F, 4.0F, 10.0F, 0.5F, 0.0F, true);
+		bipedRightArmwear.setTextureOffset(76, 50).addBox(1.001F, -2.0F, -2.0F, 0.0F, 10.0F, 4.0F, 0.0F, false);
+		
+		bipedLeftLeg = new ModelRenderer(this);
+		bipedLeftLeg.setRotationPoint(1.9F, 12.0F, 0.0F);
+		bipedLeftLeg.setTextureOffset(16, 48).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, false);
+		
+		bipedRightLeg = new ModelRenderer(this);
+		bipedRightLeg.setRotationPoint(-1.9F, 12.0F, 0.0F);
+		bipedRightLeg.setTextureOffset(16, 48).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, false);
 
-		ear_left = new ModelRenderer(this);
-		ear_left.setRotationPoint(0.0F, 5.0F, -1.0F);
-		bipedHead.addChild(ear_left);
-		ear_left.setTextureOffset(57, 10).addBox(4.0F, -8.5F, 1.0F, 0.5F, 0.5F, 1.0F, 0.0F, false);
-		ear_left.setTextureOffset(52, 10).addBox(4.5F, -8.5F, 1.0F, 0.5F, 0.5F, 1.0F, 0.0F, false);
-		ear_left.setTextureOffset(52, 10).addBox(4.0F, -8.0F, 0.5F, 0.5F, 0.5F, 1.0F, 0.0F, false);
+		toothLeft = new ModelRenderer(this);
+		toothLeft.setRotationPoint(0.0F, 5.0F, -1.0F);
+		bipedHead.addChild(toothLeft);
+		toothLeft.setTextureOffset(36, 4).addBox(1.0F, -6.0F, -3.5F, 1.0F, 1.0F, 0.5F, 0.0F, false);
+		toothLeft.setTextureOffset(36, 4).addBox(1.5F, -5.0F, -3.5F, 0.5F, 0.5F, 0.5F, 0.0F, false);
 
-		ear_right = new ModelRenderer(this);
-		ear_right.setRotationPoint(0.0F, 5.0F, -1.0F);
-		bipedHead.addChild(ear_right);
-		ear_right.setTextureOffset(52, 10).addBox(-4.5F, -8.5F, 1.0F, 0.5F, 0.5F, 1.0F, 0.0F, false);
-		ear_right.setTextureOffset(52, 10).addBox(-5.0F, -8.5F, 1.0F, 0.5F, 0.5F, 1.0F, 0.0F, false);
-		ear_right.setTextureOffset(52, 10).addBox(-4.5F, -8.0F, 0.5F, 0.5F, 0.5F, 1.0F, 0.0F, false);
+		toothRight = new ModelRenderer(this);
+		toothRight.setRotationPoint(0.0F, 5.0F, -1.0F);
+		bipedHead.addChild(toothRight);
+		toothRight.setTextureOffset(40, 4).addBox(-2.0F, -6.0F, -3.5F, 1.0F, 1.0F, 0.5F, 0.0F, false);
+		toothRight.setTextureOffset(40, 4).addBox(-2.0F, -5.0F, -3.5F, 0.5F, 0.5F, 0.5F, 0.0F, false);
 
-		bipedHeadwear = new ModelRenderer(this);
-		bipedHeadwear.setRotationPoint(0.0F, 0.0F, 0.0F);
-		bipedHeadwear.setTextureOffset(11, 6).addBox(-4.0F, -5.0F, -4.5F, 1.0F, 1.0F, 0.5F, 0.0F, false);
-		bipedHeadwear.setTextureOffset(11, 5).addBox(-2.0F, -5.0F, -4.5F, 1.0F, 1.0F, 0.5F, 0.0F, false);
-		bipedHeadwear.setTextureOffset(11, 5).addBox(1.0F, -5.0F, -4.5F, 1.0F, 1.0F, 0.5F, 0.0F, false);
-		bipedHeadwear.setTextureOffset(11, 5).addBox(3.0F, -5.0F, -4.5F, 1.0F, 1.0F, 0.5F, 0.0F, false);
-		bipedHeadwear.setTextureOffset(16, 9).addBox(-4.0F, -7.0F, -4.5F, 8.0F, 2.0F, 0.5F, 0.0F, false);
-		bipedHeadwear.setTextureOffset(20, 9).addBox(-3.0F, -8.0F, -4.5F, 6.0F, 1.0F, 0.5F, 0.0F, false);
+		earLeft = new ModelRenderer(this);
+		earLeft.setRotationPoint(0.0F, 5.0F, -1.0F);
+		bipedHead.addChild(earLeft);
+		earLeft.setTextureOffset(57, 10).addBox(4.0F, -8.5F, 1.0F, 0.5F, 0.5F, 1.0F, 0.0F, false);
+		earLeft.setTextureOffset(52, 10).addBox(4.5F, -8.5F, 1.0F, 0.5F, 0.5F, 1.0F, 0.0F, false);
+		earLeft.setTextureOffset(52, 10).addBox(4.0F, -8.0F, 0.5F, 0.5F, 0.5F, 1.0F, 0.0F, false);
 
-		bipedBodyWear = new ModelRenderer(this);
-		bipedBodyWear.setRotationPoint(0.0F, 0.0F, 0.0F);
-		bipedBodyWear.setTextureOffset(22, 23).addBox(-2.0F, 1.0F, -2.75F, 4.0F, 1.0F, 1.0F, 0.0F, false);
-		bipedBodyWear.setTextureOffset(22, 26).addBox(-2.0F, 2.0F, -2.75F, 1.0F, 1.0F, 1.0F, 0.0F, false);
-		bipedBodyWear.setTextureOffset(22, 27).addBox(1.0F, 2.0F, -2.75F, 1.0F, 1.0F, 1.0F, 0.0F, false);
-
-		bipedBodyWear.setTextureOffset(1, 37).addBox(2.0F, 0.0F, -2.75F, 2.0F, 12.0F, 0.5F, 0.0F, false);
-		bipedBodyWear.setTextureOffset(65, 37).addBox(-4.0F, 0.0F, 1.75F, 8.0F, 12.0F, 0.5F, 0.0F, false);
-		bipedBodyWear.setTextureOffset(7, 37).addBox(-4.0F, 0.0F, -2.75F, 2.0F, 12.0F, 0.5F, 0.0F, false);
+		earRight = new ModelRenderer(this);
+		earRight.setRotationPoint(0.0F, 5.0F, -1.0F);
+		bipedHead.addChild(earRight);
+		earRight.setTextureOffset(52, 10).addBox(-4.5F, -8.5F, 1.0F, 0.5F, 0.5F, 1.0F, 0.0F, false);
+		earRight.setTextureOffset(52, 10).addBox(-5.0F, -8.5F, 1.0F, 0.5F, 0.5F, 1.0F, 0.0F, false);
+		earRight.setTextureOffset(52, 10).addBox(-4.5F, -8.0F, 0.5F, 0.5F, 0.5F, 1.0F, 0.0F, false);
 	}
 	
 	@Override
@@ -115,12 +116,6 @@ public class VampirePlayerModel extends AbstractMMOPlayerModel {
 	@Override
 	protected Iterable<ModelRenderer> getBodyParts() {
 		return ImmutableList.of(bipedBody, bipedBodyWear, bipedHeadwear, bipedRightLeg, bipedLeftLeg, bipedLeftArm, bipedRightArm, bipedLeftArmwear, bipedRightArmwear);
-	}
-	
-	@Override
-	public void render(MatrixStack matrixStackIn, IVertexBuilder bufferIn, int packedLightIn, int packedOverlayIn,
-			float red, float green, float blue, float alpha) {
-		super.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn, red, green, blue, alpha);
 	}
 
 	@Override
