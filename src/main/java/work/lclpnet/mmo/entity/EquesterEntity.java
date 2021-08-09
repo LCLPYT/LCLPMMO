83b044b7848be1a8eb81ca850fe4a0166157de81
DerBerliner <34981166+DerBerlinr@users.noreply.github.com>
2021-08-09 20:22:50 +0200

    added Dolphin-like behaviour

--- src/main/java/work/lclpnet/mmo/entity/EquesterEntity.java
+++ src/main/java/work/lclpnet/mmo/entity/EquesterEntity.java
@@ -1,15 +1,17 @@
 package work.lclpnet.mmo.entity;
 
+import net.minecraft.entity.EntityPredicate;
 import net.minecraft.entity.EntitySize;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.Pose;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
-import net.minecraft.entity.ai.goal.MeleeAttackGoal;
-import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
-import net.minecraft.entity.ai.goal.SwimGoal;
+import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.monster.GuardianEntity;
 import net.minecraft.entity.monster.MonsterEntity;
+import net.minecraft.entity.passive.WaterMobEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -22,7 +24,12 @@ import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 
-public class EquesterEntity extends MonsterEntity implements IAnimatable {
+import java.util.EnumSet;
+
+public class EquesterEntity extends WaterMobEntity implements IAnimatable {
+
+
+    private static final EntityPredicate field_213810_bA = (new EntityPredicate()).setDistance(10.0D).allowFriendlyFire().allowInvulnerable().setIgnoresLineOfSight();
 
     /*  Client-Only Fields
     ! IMPORTANT !
@@ -46,11 +53,8 @@ public class EquesterEntity extends MonsterEntity implements IAnimatable {
 
     public static AttributeModifierMap.MutableAttribute prepareAttributes() {
         return MonsterEntity.func_234295_eP_()
-                .createMutableAttribute(Attributes.FOLLOW_RANGE, 30.0D)
-                .createMutableAttribute(Attributes.MOVEMENT_SPEED, 1.5D)
-                .createMutableAttribute(Attributes.ATTACK_DAMAGE, 9.0D)
-                .createMutableAttribute(Attributes.ARMOR, 5.0D)
-                .createMutableAttribute(Attributes.KNOCKBACK_RESISTANCE, 0.5D);
+                .createMutableAttribute(Attributes.MOVEMENT_SPEED, 2D)
+                .createMutableAttribute(Attributes.ATTACK_DAMAGE, 9.0D);
     }
 
     @SuppressWarnings("ConstantConditions")
@@ -62,14 +66,16 @@ public class EquesterEntity extends MonsterEntity implements IAnimatable {
 
     @Override
     protected void registerGoals() {
-        this.goalSelector.addGoal(10, new SwimGoal(this));
-        this.applyEntityAI();
-    }
-
-    protected void applyEntityAI() {
-        this.goalSelector.addGoal(3, new MeleeAttackGoal(this, 1.0D, false));
-        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, PlayerEntity.class, false));
-        this.targetSelector.addGoal(4, new NearestAttackableTargetGoal<>(this, LivingEntity.class, true));
+        this.goalSelector.addGoal(0, new BreatheAirGoal(this));
+        this.goalSelector.addGoal(0, new FindWaterGoal(this));
+        this.goalSelector.addGoal(1, new SwimGoal(this));
+        this.goalSelector.addGoal(2, new EquesterEntity.SwimWithPlayerGoal(this, 4.0D));
+        this.goalSelector.addGoal(4, new RandomSwimmingGoal(this, 1.0D, 10));
+        this.goalSelector.addGoal(4, new LookRandomlyGoal(this));
+        this.goalSelector.addGoal(5, new LookAtGoal(this, PlayerEntity.class, 6.0F));
+        this.goalSelector.addGoal(6, new MeleeAttackGoal(this, 1.2F, true));
+        this.goalSelector.addGoal(8, new FollowBoatGoal(this));
+        this.goalSelector.addGoal(9, new AvoidEntityGoal<>(this, GuardianEntity.class, 8.0F, 1.0D, 1.0D));
     }
 
     @Override
@@ -90,5 +96,68 @@ public class EquesterEntity extends MonsterEntity implements IAnimatable {
     @Override
     protected float getStandingEyeHeight(Pose poseIn, EntitySize sizeIn) {
         return 2.85F;
+    }
+
+    static class SwimWithPlayerGoal extends Goal {
+        private final EquesterEntity equester;
+        private final double speed;
+        private PlayerEntity targetPlayer;
+
+        SwimWithPlayerGoal(EquesterEntity equesterIn, double speedIn) {
+            this.equester = equesterIn;
+            this.speed = speedIn;
+            this.setMutexFlags(EnumSet.of(Goal.Flag.MOVE, Goal.Flag.LOOK));
+        }
+
+        /**
+         * Returns whether execution should begin. You can also read and cache any state necessary for execution in this
+         * method as well.
+         */
+        public boolean shouldExecute() {
+            this.targetPlayer = this.equester.world.getClosestPlayer(EquesterEntity.field_213810_bA, this.equester);
+            if (this.targetPlayer == null) {
+                return false;
+            } else {
+                return this.targetPlayer.isSwimming() && this.equester.getAttackTarget() != this.targetPlayer;
+            }
+        }
+
+        /**
+         * Returns whether an in-progress EntityAIBase should continue executing
+         */
+        public boolean shouldContinueExecuting() {
+            return this.targetPlayer != null && this.targetPlayer.isSwimming() && this.equester.getDistanceSq(this.targetPlayer) < 256.0D;
+        }
+
+        /**
+         * Execute a one shot task or start executing a continuous task
+         */
+        public void startExecuting() {
+            this.targetPlayer.addPotionEffect(new EffectInstance(Effects.DOLPHINS_GRACE, 100));
+        }
+
+        /**
+         * Reset the task's internal state. Called when this task is interrupted by another one
+         */
+        public void resetTask() {
+            this.targetPlayer = null;
+            this.equester.getNavigator().clearPath();
+        }
+
+        /**
+         * Keep ticking a continuous task that has already been started
+         */
+        public void tick() {
+            this.equester.getLookController().setLookPositionWithEntity(this.targetPlayer, (float) (this.equester.getHorizontalFaceSpeed() + 20), (float) this.equester.getVerticalFaceSpeed());
+            if (this.equester.getDistanceSq(this.targetPlayer) < 6.25D) {
+                this.equester.getNavigator().clearPath();
+            } else {
+                this.equester.getNavigator().tryMoveToEntityLiving(this.targetPlayer, this.speed);
+            }
+
+            if (this.targetPlayer.isSwimming() && this.targetPlayer.world.rand.nextInt(6) == 0) {
+                this.targetPlayer.addPotionEffect(new EffectInstance(Effects.DOLPHINS_GRACE, 100));
+            }
+        }
     }
 }
