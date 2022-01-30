a9fa0b05165ada3afa7e3bacec39c5bbfac763d2
LCLP <lukas@lclpnet.work>
2022-01-30 22:31:03 +0100

    reimplemented logout, fixed SystemToast singleline

--- src/main/java/work/lclpnet/mmo/client/util/SingleRequestManager.java
+++ src/main/java/work/lclpnet/mmo/client/util/SingleRequestManager.java
@@ -1,11 +1,16 @@
 package work.lclpnet.mmo.client.util;
 
+import com.google.gson.JsonObject;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import work.lclpnet.lclpnetwork.api.APIException;
+import work.lclpnet.lclpnetwork.api.APIResponse;
+import work.lclpnet.lclpnetwork.api.ResponseEvaluationException;
 import work.lclpnet.mmo.network.backend.MMOAPI;
 
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 
 @Environment(EnvType.CLIENT)
 public class SingleRequestManager {
@@ -26,6 +31,17 @@ public class SingleRequestManager {
                 if (err != null) err.printStackTrace(); // Saving the token failed, but the token is still valid
                 return true;
             });
+        }).exceptionally(err -> {
+            Throwable ex = err instanceof CompletionException ? err.getCause() : err;
+            if (ex == null) ex = err;
+
+            if (ex instanceof ResponseEvaluationException) {
+                APIResponse resp = ((ResponseEvaluationException) ex).getResponse();
+                if (resp.getResponseCode() == 401) return false;
+            }
+
+            // unknown cause
+            throw err instanceof CompletionException ? (CompletionException) err : new CompletionException(err);
         }).whenComplete((result, err) -> locked = false);
     }
 }
