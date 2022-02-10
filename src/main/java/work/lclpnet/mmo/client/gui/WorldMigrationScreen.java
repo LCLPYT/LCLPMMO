e367470dfe2e0bef21c7292ec11ee10cf27804eb
LCLP <lukas@lclpnet.work>
2022-02-10 21:15:41 +0100

    world region files can now be read and written

--- src/main/java/work/lclpnet/mmo/client/gui/WorldMigrationScreen.java
+++ src/main/java/work/lclpnet/mmo/client/gui/WorldMigrationScreen.java
@@ -3,6 +3,7 @@ package work.lclpnet.mmo.client.gui;
 import net.minecraft.client.gui.screen.world.EditWorldScreen;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
 import net.minecraft.world.level.storage.LevelStorage;
 import work.lclpnet.mmo.client.util.Color;
 import work.lclpnet.mmo.migrator.WorldMigrator;
@@ -10,14 +11,17 @@ import work.lclpnet.mmo.migrator.WorldMigrator;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-public class WorldMigrationScreen extends MMOScreen {
+public class WorldMigrationScreen extends MMOScreen implements WorldMigrator.ProgressListener {
 
     protected final LevelStorage.Session session;
     protected final AtomicBoolean aBoolean = new AtomicBoolean(false);
     protected long lastDotTick = -1;
     protected int dots = 3;
     protected final EditWorldScreen parent;
-    protected String text = getText();
+    protected int steps = 0, currentStep = 0;
+    protected Text text = getText();
+    protected String progressText = getProgressText();
+    protected int percentHash = 0;
 
     public WorldMigrationScreen(EditWorldScreen parent, LevelStorage.Session session) {
         super(new LiteralText("World Migration"));
@@ -32,7 +36,7 @@ public class WorldMigrationScreen extends MMOScreen {
     }
 
     protected void startMigration() {
-        WorldMigrator.create(Objects.requireNonNull(client), session)
+        WorldMigrator.create(Objects.requireNonNull(client), session, this)
                 .thenCompose(WorldMigrator::migrate)
                 .thenRun(() -> aBoolean.set(true));
     }
@@ -58,14 +62,43 @@ public class WorldMigrationScreen extends MMOScreen {
             this.text = this.getText();
         }
 
-        drawCenteredString(matrices, this.client.textRenderer, this.text, this.width / 2, this.height / 2, Color.WHITE);
+        drawMultiLineCenteredString(matrices, this.client.textRenderer, this.text, 1.5F, this.width / 2, this.height / 2 - 20, Color.AQUA);
+        drawCenteredString(matrices, this.client.textRenderer, this.progressText, this.width / 2, this.height / 2, Color.WHITE);
 
         super.render(matrices, mouseX, mouseY, delta);
     }
 
-    protected String getText() {
+    protected Text getText() {
         final StringBuilder builder = new StringBuilder("Migration in progress");
         for (int i = 0; i < dots; i++) builder.append('.');
+        return new LiteralText(builder.toString());
+    }
+
+    protected String getProgressText() {
+        StringBuilder builder = new StringBuilder(String.format("%.2f%% done", percentHash / 10e+2));
+        if (steps > 0) builder.append(String.format(" (Dimension %s/%s)", currentStep, steps));
         return builder.toString();
+    }
+
+    @Override
+    public void setSteps(int steps) {
+        this.steps = steps;
+    }
+
+    @Override
+    public void updateCurrentStep(int currentStep) {
+        this.currentStep = currentStep;
+        this.percentHash = 0;
+        this.progressText = getProgressText();
+    }
+
+    @Override
+    public void updateProgress(float progress) {
+        int hash = (int) (progress * 10e+4);
+
+        if (hash > percentHash) {
+            percentHash = hash;
+            this.progressText = getProgressText();
+        }
     }
 }
--- src/main/java/work/lclpnet/mmo/migrator/WorldMigrator.java
+++ src/main/java/work/lclpnet/mmo/migrator/WorldMigrator.java
@@ -1,39 +1,56 @@
 package work.lclpnet.mmo.migrator;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Lists;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtIo;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.registry.DynamicRegistryManager;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.SaveProperties;
 import net.minecraft.world.World;
 import net.minecraft.world.level.LevelInfo;
 import net.minecraft.world.level.storage.LevelStorage;
+import net.minecraft.world.storage.RegionFile;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import work.lclpnet.mmo.client.gui.WorldMigrationScreen;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WorldMigrator {
 
     private static final Logger LOGGER = LogManager.getLogger();
+    private static final Pattern REGION_FILE_PATTERN = Pattern.compile("^r\\.(-?[0-9]+)\\.(-?[0-9]+)\\.mca$");
 
     protected final LevelInfo levelInfo;
     protected final LevelStorage.Session session;
     protected final ImmutableSet<RegistryKey<World>> worlds;
+    protected final ProgressListener progressListener;
 
-    protected WorldMigrator(LevelStorage.Session session, LevelInfo levelInfo, ImmutableSet<RegistryKey<World>> worlds) {
+    protected WorldMigrator(LevelStorage.Session session, LevelInfo levelInfo, ImmutableSet<RegistryKey<World>> worlds, ProgressListener progressListener) {
         this.session = Objects.requireNonNull(session);
         this.levelInfo = Objects.requireNonNull(levelInfo);
         this.worlds = Objects.requireNonNull(worlds);
+        this.progressListener = Objects.requireNonNull(progressListener);
+        this.progressListener.setSteps(worlds.size());
     }
 
-    public static CompletableFuture<WorldMigrator> create(MinecraftClient client, LevelStorage.Session storageSession) {
-        return CompletableFuture.supplyAsync(() -> createSync(client, storageSession));
+    public static CompletableFuture<WorldMigrator> create(MinecraftClient client, LevelStorage.Session storageSession, ProgressListener progressListener) {
+        return CompletableFuture.supplyAsync(() -> createSync(client, storageSession, progressListener));
     }
 
-    protected static WorldMigrator createSync(MinecraftClient client, LevelStorage.Session storageSession) {
+    protected static WorldMigrator createSync(MinecraftClient client, LevelStorage.Session storageSession, ProgressListener progressListener) {
         DynamicRegistryManager.Impl impl = DynamicRegistryManager.create();
 
         try {
@@ -44,7 +61,7 @@ public class WorldMigrator {
                 SaveProperties saveProperties = integratedResourceManager.getSaveProperties();
                 storageSession.backupLevelDataFile(impl, saveProperties);
                 ImmutableSet<RegistryKey<World>> immutableSet = saveProperties.getGeneratorOptions().getWorlds();
-                migrator = new WorldMigrator(storageSession, saveProperties.getLevelInfo(), immutableSet);
+                migrator = new WorldMigrator(storageSession, saveProperties.getLevelInfo(), immutableSet, progressListener);
             } finally {
                 if (integratedResourceManager != null) {
                     integratedResourceManager.close();
@@ -59,12 +76,115 @@ public class WorldMigrator {
     }
 
     public CompletableFuture<Void> migrate() {
-        return CompletableFuture.runAsync(() -> {
-            try {
-                Thread.sleep(10000L);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        return CompletableFuture.runAsync(this::migrateSync);
+    }
+
+    protected void migrateSync() {
+        final ImmutableList<RegistryKey<World>> list = this.worlds.asList();
+
+        final int dimCount = worlds.size();
+        LOGGER.info("Found {} dimensions", dimCount);
+
+        for (int i = 0; i < dimCount; i++) {
+            this.progressListener.updateCurrentStep(i + 1);
+            this.migrateWorld(list.get(i));
+        }
+
+        LOGGER.info("Migration complete.");
+    }
+
+    protected void migrateWorld(RegistryKey<World> world) {
+        LOGGER.info("Migrating world {}...", world.getValue());
+
+        File worldDirectory = this.session.getWorldDirectory(world);
+        File regionDirectory = new File(worldDirectory, "region");
+        File[] files = regionDirectory.listFiles((filex, string) -> string.endsWith(".mca"));
+        if (files == null) {
+            LOGGER.info("No region files found in {}", regionDirectory.getAbsolutePath());
+            return;
+        }
+
+        List<RegionFileLocation> regionFiles = new ArrayList<>();
+
+        for (File regionFile : files) {
+            Matcher matcher = REGION_FILE_PATTERN.matcher(regionFile.getName());
+            if (!matcher.matches()) continue;
+
+            int regionX = Integer.parseInt(matcher.group(1)) << 5;
+            int regionY = Integer.parseInt(matcher.group(2)) << 5;
+
+            regionFiles.add(new RegionFileLocation(regionFile, regionX, regionY));
+        }
+
+        final int regionFileCount = regionFiles.size();
+        LOGGER.info("Found {} region files.", regionFileCount);
+
+        for (int i = 0; i < regionFileCount; i++) {
+            RegionFileLocation regionFile = regionFiles.get(i);
+            migrateRegionFile(regionFile, regionDirectory);
+            this.progressListener.updateProgress((i + 1) / (float) regionFileCount);
+        }
+
+        LOGGER.info("World {} migrated successfully.", world.getValue());
+    }
+
+    protected void migrateRegionFile(RegionFileLocation region, File regDirectory) {
+        LOGGER.info("Migrating region file {}...", region.file.getName());
+
+        try (RegionFile regionFile = new RegionFile(region.file, regDirectory, true)) {
+            List<ChunkPos> chunkPositions = Lists.newArrayList();
+
+            for (int x = 0; x < 32; ++x) {
+                for (int y = 0; y < 32; ++y) {
+                    ChunkPos chunkPos = new ChunkPos(x + region.x, y + region.y);
+
+                    if (regionFile.isChunkValid(chunkPos))
+                        chunkPositions.add(chunkPos);
+                }
             }
-        });
+
+            for (ChunkPos chunkPosition : chunkPositions) {
+                migrateChunk(regionFile, chunkPosition);
+            }
+        } catch (Throwable ignored) {}
+    }
+
+    protected void migrateChunk(RegionFile regionFile, ChunkPos chunkPos) throws IOException {
+        CompoundTag chunkTag;
+        try (DataInputStream chunkIn = regionFile.getChunkInputStream(chunkPos)) {
+            if (chunkIn == null) {
+                LOGGER.info("Failed to fetch input stream for chunk {}", chunkPos);
+                return;
+            }
+
+            chunkTag = NbtIo.read(chunkIn);
+        }
+
+        migrateChunkTag(chunkTag);
+
+        try (DataOutputStream chunkOut = regionFile.getChunkOutputStream(chunkPos)) {
+            NbtIo.write(chunkTag, chunkOut);
+        }
+    }
+
+    protected void migrateChunkTag(CompoundTag chunkTag) {
+
+    }
+
+    public static class RegionFileLocation {
+        public final File file;
+        public final int x, y;
+
+        public RegionFileLocation(File file, int x, int y) {
+            this.file = file;
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public interface ProgressListener {
+        void setSteps(int steps);
+        void updateCurrentStep(int currentStep);
+        void updateProgress(float progress);
     }
 }
